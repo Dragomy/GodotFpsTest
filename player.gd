2eec28fb50c3f66d7a309a6f2a5956f72e9b3a2c
Dragomy <95567734+Dragomy@users.noreply.github.com>
2024-10-17 00:47:56 +0200

    Movement
    Just the Movement from my movement test repo to get more of a game feel

--- player.gd
+++ player.gd
@@ -1,47 +1,56 @@
 extends CharacterBody3D
 
-#- @onready ----------
+
+#- Camera ----------
 @onready var camera = $Camera
-@onready var ray_left = $Raycast/Left
-@onready var ray_right = $Raycast/Right
-@onready var ray_down = $Raycast/Down
-
-@onready var smg = $Camera/gun/smg
-@onready var flamethrower = $Camera/gun/flamethrower
-
-@onready var active_gun =[smg,flamethrower] 
-
-#- const ----------
-const gravity = 9.81 * 2
-const BASE_SPEED = 8.0
-const ACCELERATION = 4
-const JUMP_VELOCITY = 10.0
-const max_jumps = 3
-
-#- var: boolean ----------
 var tilt_on: bool = true
 var wobble_on: bool = true
-var is_wall_sliding: bool = false
-
-#- var: float -------
-var current_speed = BASE_SPEED
-var mouse_sensitivity = 0.006
 var wobble_amplitude := 0.03  # Maximum height of the wobble
 var wobble_base_speed := 5.0  # Base speed of the wobble
 var base_camera_height := 0.0  # To store the camera's initial Y position
 var wobble_time := 0.0  # To keep track of time
-
-var jump_count
 var tilt_angle = 1.9
 
+
+#- Envoirenment ----------
+const gravity = 9.81 * 2
+
+
+#- Movement ----------
+@onready var ray_left = $Raycast/Left
+@onready var ray_right = $Raycast/Right
+@onready var ray_down = $Raycast/Down
+const BASE_SPEED = 8.0
+const ACCELERATION = 10.0
+const JUMP_VELOCITY = 10.0
+const max_jumps = 3
+var is_wall_sliding: bool = false
+var is_running: bool = false
+var is_groundslaming: bool = false
+var current_speed = BASE_SPEED
+var jump_count
+var save_crouch_input_speed
+var is_crouching = false
+var runspeed = BASE_SPEED * 2
+
+
+#- Weapons ----------
+@onready var smg = $Camera/gun/smg
+@onready var flamethrower = $Camera/gun/flamethrower
+@onready var active_gun =[smg,flamethrower] 
 var array_index = 0
 
+
+#- Input ----------
+var mouse_sensitivity = 0.006
+
+
+#- StandartFunctions ----------
 func _ready():
-	# Capture Mouse Movement
+	#Capture Mouse Movement
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	# Store the initial camera height
 	base_camera_height = camera.position.y  
-
 
 func _unhandled_input(event):
 	# Handle Mouse Movement
@@ -63,30 +72,35 @@ func _unhandled_input(event):
 				array_index -= 1
 				active_gun[array_index].show()
 
-
 func _physics_process(delta):
-	# Close game on ui_escape 
+	#Close game on ui_escape 
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
 
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-	
-	handle_weapon()  # Call shoot here
+		
 	#- Respawn ----------
 	respawn()
+	
 	#- Movement ----------
 	walk(delta)
 	run()
 	jump()
+	crouch()
+	groundslam()
 	wallrun()
+	handle_weapon()
+	
 	#- Camera ----------
 	tiltCamera()
-	wobbleCamera(delta, current_speed)
-
-	move_and_slide()
+	wobbleCamera(delta,current_speed)
 	
-			
+	#- MoveAndSlide ----------
+	move_and_slide()
+
+
+#- WeaponHandling ----------
 func handle_weapon():
 	active_gun[array_index].shoot()
 	active_gun[array_index].reload()
@@ -100,20 +114,20 @@ func handle_weapon():
 		array_index = 1
 		active_gun[array_index].show()
 
+
 #- Respawn ----------
 func respawn():
 	if Input.is_action_just_pressed("respawn"):
 		global_transform.origin = Vector3(0, 0, 0)
 		velocity = Vector3.ZERO 
 
+
 #- Movement ----------
 func walk(delta):
-	# Get the input direction and handle the movement/deceleration
 	var input_dir = Input.get_vector("left", "right", "forwards", "backwards")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if direction:
-		# Apply movement
 		pass
 	else:
 		velocity.x = move_toward(velocity.x, 0, current_speed)
@@ -123,7 +137,6 @@ func walk(delta):
 	velocity = velocity.lerp(direction * current_speed, ACCELERATION * delta)
 	velocity.y = y_velocity
 
-var runspeed = BASE_SPEED * 2
 func run():
 	if Input.is_action_pressed("run"):
 		current_speed = runspeed
@@ -137,12 +150,37 @@ func jump():
 	elif is_on_floor():
 		jump_count = 0
 
+func crouch():
+	if Input.is_action_just_pressed("crouch") && !is_running && !is_crouching:
+		is_crouching = true
+		scale.y = 0.5
+		save_crouch_input_speed = current_speed
+		current_speed = current_speed / 2
+	elif Input.is_action_just_pressed("crouch") && Input.is_action_just_pressed("run"):
+		scale.y = 1
+		current_speed = save_crouch_input_speed
+		is_crouching = false
+	elif Input.is_action_just_released("crouch") && is_crouching:
+		scale.y = 1
+		current_speed = save_crouch_input_speed
+		is_crouching = false
+
+func groundslam():
+	if !is_on_floor():
+		if Input.is_action_just_pressed("slam"):
+			is_groundslaming = true
+			velocity.y = -100
+	elif is_on_floor() && is_groundslaming:
+		is_groundslaming = false
+		#slam_animation.play()
+
 func wallrun():
 	if (ray_left.is_colliding() || ray_right.is_colliding()) && !ray_down.is_colliding():
 		velocity.y = -0.8
 		is_wall_sliding = true
 	else: 
 		is_wall_sliding = false
+
 
 #- Camera ----------
 func tiltCamera():
